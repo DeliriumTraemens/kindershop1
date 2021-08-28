64437beaa7f64dbfe7f84049aa98699c4502b247
DeliriumTraemens <oskinnick@gmail.com>
2021-08-28 12:25:44 +0300

    Category and Properties Picture working With Configurer SubCategories and Products added

--- src/main/java/org/nick/kindershop1/jdbc/rowMappers/ImageMapper.java
+++ src/main/java/org/nick/kindershop1/jdbc/rowMappers/ImageMapper.java
@@ -13,18 +13,8 @@ public class ImageMapper implements RowMapper<List> {
 	@Override
 	public List mapRow(ResultSet rs, int rowNum) throws SQLException {
 		
-		System.out.println("\n>>>>>>>>>>>>>>>>>>"+rs.getMetaData());
-		Product imageMapProd = new Product();
 		List <String> images = new ArrayList();
-		
 			images.add(rs.getString("image"));
-//		while (rs.next()) {
-//		}
-		imageMapProd.setImgArray(images);
-		System.out.println("\n<<<<<<<<<<<<<<<"+images);
-		
-		
-//		return imageMapProd;
 		return images;
 	}
 }
